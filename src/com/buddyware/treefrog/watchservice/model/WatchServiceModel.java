8b231e4561af2fd0f12629da4a697f1995563e02
graffy76 <monograff76@gmail.com>
2014-08-25 21:10:17 -0500

    Began implementation of directory wawtch service module

---
+++ src/com/buddyware/treefrog/watchservice/model/WatchServiceModel.java
@@ -0,0 +1,12 @@
+package com.buddyware.treefrog.watchservice.model;
+
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.util.Map;
+
+public class WatchServiceModel {
+
+		public Map<WatchKey,Path> getFilesystemKeys() {
+			return null;
+		}
+}
