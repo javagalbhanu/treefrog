28b09d3905917345e95a803e418c2cd33f1c32d5
graffy76 <monograff76@gmail.com>
2014-08-27 09:59:02 -0500

    Implemented loading of separate views into tab pane

--- src/com/buddyware/treefrog/Main.java
+++ src/com/buddyware/treefrog/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
+import com.buddyware.treefrog.stats.StatsController;
 import com.buddyware.treefrog.util.utils;
 
 public class Main extends Application {
@@ -16,6 +17,8 @@ public class Main extends Application {
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	
+	//@FXML
+	private StatsController stats_tab_view;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -25,46 +28,33 @@ public class Main extends Application {
 		
 		//static reference for all controllers to provide access back to main, if needed.
 		BaseController.mMain = this;
-		/*
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-		} catch(Exception e) {
-			e.printStackTrace();
-		}*/
-		
 		utils. <BorderPane, BaseController> loadFxml ("RootLayout.fxml", this.primaryStage);
 	}
 	
 	@FXML
-	public void showDirectoryWatchServiceView() {
+	public void showStatsTabView() {
+	
+	}
+	
+	@FXML
+	public void showRemoteConfigTabView() {
+	
+	}
+	
+	@FXML
+	public void showLocalConfigTabView() {
 		
 	}
 	
 	@FXML
-	public void showJavaFxDemoView() {
+	public void showHelpTabView() {
+		
+	}
 	
-		try {
-			
-			//load the aws demo code view
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation (Main.class.getResource("awsdemo/view/AwsDemoView.fxml"));
-			AnchorPane awsDemoView = (AnchorPane) loader.load();
-			
-	//		AwsDemoController ctl = loader.getController();
-			
-	//		ctl.setParentStage (primaryStage);
-			
-			//set the view in the center of the root layout
-			rootLayout.setCenter (awsDemoView);
-			
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-			
-		}
+	@FXML
+	public void showAboutTabView() {
+		
 	}
 	
 	public Stage getPrimaryStage() {
